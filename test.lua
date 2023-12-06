5a15603d55d00508f100af2411e31d9e94d3d983
Marton Lederer <marton@lederer.hu>
2023-12-06 14:10:59 +0100

    feat: structure

--- test.lua
+++ test.lua
@@ -1,3 +1,25 @@
 local Type = require("type")
 
-local address = Type:string()
+local address = Type:number()
+
+address:assert(2)
+
+local test = Type:boolean()
+
+test:assert(false)
+
+local user = Type:structure({
+  name = Type:string(),
+  age = Type:number():integer(),
+  social = Type:structure({
+    twitter = Type:string()
+  })
+}, "User")
+
+user:assert({
+  name = "test",
+  age = 20,
+  social = {
+    twitter = "martonlederer"
+  }
+})
--- type.lua
+++ type.lua
@@ -1,4 +1,7 @@
-local Type = {}
+---@class Type
+local Type = {
+  conditions = nil
+}
 
 function Type:add_condition(name, assertion)
   -- condition to add
@@ -110,12 +113,60 @@ function Type:greater_than(n)
   return self:add_condition("less", function (val) return val > n end)
 end
 
+-- Table must be of structure
+---@param struct { [any]: Type }
+---@param name? string Name of the structure
+---@param strict? boolean Only allow the defined keys from the structure, throw error on other keys (false by default)
+function Type:structure(struct, name, strict)
+  if type(struct) ~= "table" then
+    error(name .. " is not a valid structure:\n" .. tostring(struct))
+  end
+
+  return self:add_condition(
+    name or "structure",
+    function (val)
+      if type(val) ~= "table" then
+        print("Not a valid structure:\n" .. tostring(val))
+        return false
+      end
+
+      -- for each value, validate
+      for key, assertion in pairs(struct) do
+        if val[key] == nil then
+          print("Missing key \"" .. key .. "\"")
+          return false
+        end
+
+        -- check if the assertion throws any errors
+        local success, err = pcall(function () return assertion:assert(val[key]) end)
+
+        if not success then
+          print(err);
+          return false
+        end
+      end
+
+      -- in strict mode, we do not allow any other keys
+      if strict then
+        for key, _ in pairs(val) do
+          if struct[key] == nil then
+            print("Invalid key in value: \"" .. key .. "\" (strict mode)");
+            return false
+          end
+        end
+      end
+
+      return true
+    end
+  )
+end
+
 -- Execute an assertion for a given value
 ---@param val any Value to assert for
 function Type:assert(val)
   for i, condition in ipairs(self.conditions) do
     if not condition.validate(val) then
-      error("Failed validation at #" .. i .. ": not " .. condition.name)
+      error("Failed assertion at #" .. i .. ": " .. tostring(val) .. " is not " .. condition.name)
     end
   end
 end
